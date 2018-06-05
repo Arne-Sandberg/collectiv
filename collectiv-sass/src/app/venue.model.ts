91047a43ae6495c1a1bb4d2858414944c3b82c41
Guest User <Guest@Epicodus-3E.local>
2018-06-05 15:44:04 -0700

    add venue.model to prep dynamic routes

---
+++ collectiv-sass/src/app/venue.model.ts
@@ -0,0 +1,4 @@
+export class Venue {
+  constructor (public location:string, public description:string, public image:string,public id:number){ }
+
+}
--- collectiv-sass/src/app/venues/venues.component.ts
+++ collectiv-sass/src/app/venues/venues.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Venue } from '../venue.model';
 
 @Component({
   selector: 'app-venues',
@@ -11,5 +12,10 @@ export class VenuesComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  venuelist: Venue[] = [
+    new Venue("Southeast", "This cozy and homey workspace is part of a historic neighborhood near Hawthorne boulevard near many wonderful places to break for lunch.", "../assets/workplace se.jpg", 1),
+    new Venue("Northeast", "Located on NE Alberta street, this area is known for being artistic and quirky, perfect for inspiring walks between meetings", "../assets/workspace-ne.jpeg", 2),
+    new Venue("Downtown", "Our downtown office is a great choice for working, as it provides the comfort and inspiring atmosphere of our other venues while also being within walking distance of other corporate offices and buildings and dozens of food carts","../assets/workspace-downtown.jpg", 3),
+    new Venue("Pearl District", "The Pearl District, located in the northwest quadrant of town is a popular and hip neighborhood","../assets/workspace-pear.jpeg",4)
+  ]
 }
